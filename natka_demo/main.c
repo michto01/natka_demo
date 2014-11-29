985ccc44cbdd7ba31dd33a7ef04dc127b5d6490c
Tomas Michalek <tomas.michalek@gatema.cz>
2014-11-29 18:39:55 +0100

    Initial commit
    Quick and dirty C tutorial
    
    Signed-off-by: Tomas Michalek <tomas.michalek@gatema.cz>

--- natka_demo/main.c
+++ natka_demo/main.c
@@ -1,15 +1,56 @@
-//
-//  main.c
-//  natka_demo
-//
-//  Created by Tomas Michalek on 29/11/14.
-//  Copyright (c) 2014 CRE8IV.EU. All rights reserved.
-//
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+#define m 3;
+
+const double CONVERSION_ERROR = -1.0;
+
+const double EURO  = 25.0;
+const double LIBRA = 34.0;
+
+enum {
+    _EURO,
+    _LIBRA,
+    KORUNA,
+};
+
+int getType(char* str)
+{
+    if (strcmp(str,"eur")) {
+        return _EURO;
+    }
+    return -1;
+}
+
+double convert ( int amount, char valut )
+{
+    switch (valut) {
+        case 'e':
+        case 'E':
+            return amount * EURO;
+            
+        case 'l':
+        case 'L':
+            return amount * LIBRA * m;
+            
+        default:
+            return CONVERSION_ERROR;
+    }
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    double amount;
+    unsigned char value;
+    
+    printf("Give value and valut:\n");
+    scanf("%lf %c", &amount, &value);
+    getchar();
+    
+    assert(convert(amount, value) != CONVERSION_ERROR);
+    printf("Valut has int value: %d \n", value);
+    printf("Converted value is: %lf", convert( amount, value ));
+    getchar();
+    return EXIT_SUCCESS;
 }
