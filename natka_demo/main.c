d1eef5b6786c6b166b06f226d59b48dac031a9bc
Tomas Michalek <tomas.michalek@gatema.cz>
2014-11-29 18:04:21 +0100

    Initial Commit

---
+++ natka_demo/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  natka_demo
+//
+//  Created by Tomas Michalek on 29/11/14.
+//  Copyright (c) 2014 CRE8IV.EU. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
